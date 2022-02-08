0eba40ea4fab12a94327efe6c02412860f1c18b0
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2022-02-08 10:51:01 -0500

    Migrated server code

--- api/recaptcha.js
+++ api/recaptcha.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const router = express.Router();
+var request = require('request');
 
 router.get('/', async (req, res) => {
     try {
         res.json({
             status: 200,
             message: 'GET data successful'
+        });
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
+})
+
+router.post('/', async function(req, res) {
+    try {
+        let response = req.body.token;
+        let secret = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? process.env.REACT_APP_RECAPTCHA_SERVER_KEY_LOCAL : process.env.REACT_APP_RECAPTCHA_SERVER_KEY;
+        let api = 'https://www.google.com/recaptcha/api/siteverify?secret=' + secret + '&response=' + response
+
+        request(api, function(error, response, body){
+            if(error) {
+                console.error(error);
+            }
+            else {
+                res.statusCode = 200;
+                res.send(body);
+            }
         });
     }
     catch (error) {
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const recaptcha = require('./api/recaptcha');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json({
+    type: "*/*"    
+}));
+dotenv.config({path: './.env'});
 
 app.use(express.json({extended: false}));
 
 app.use('/api/recaptcha', recaptcha);
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log('Server is running on port: ${PORT}'));
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
